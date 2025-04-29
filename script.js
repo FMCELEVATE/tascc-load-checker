404d8b645c3ab43217772ee75de250e318d897c8
FMCELEVATE <gary@fmchaulage.ie>
2025-04-29 22:28:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+fetch(https://script.google.com/macros/library/d/1UO71oayajbclNO5uj486ln4F8OFdedRdLH8D-N2oQuER5BbI3PleJncs/1", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+})
 // Final JavaScript for TASCC Portal
