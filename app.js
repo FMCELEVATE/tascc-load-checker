7a57cebdbf655f6d204c3d330f3f26346efc272e
FMCELEVATE <gary@fmchaulage.ie>
2025-04-29 15:07:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
-const SCRIPT_URL = "YOUR_SCRIPT_URL_HERE";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbze1DEQHOsq7XNFHhMqBIt9cQsd4ecDFtcOK3wmTrVNlxP0KE2ekIEAtS_BTMpvQh8P/exec";
 function searchTrailer() { console.log('Search logic here'); }
